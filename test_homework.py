e439f7c90b25074d7d359e3ff0a8a40f1a55c10b
Valeriy Nasyrov <valerius.nasyrov@yandex.ru>
2025-08-19 21:12:05 +0300

    Решил задачу test_unique_elements

--- test_homework.py
+++ test_homework.py
@@ -71,6 +71,8 @@ def test_unique_elements():
     Удалите из списка все повторяющиеся элементы
     """
     l = [1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 8, 9, 10, 10]
+    l = list(set(l))
+    print(l)
     # TODO удалите повторяющиеся элементы
 
     assert isinstance(l, list)
