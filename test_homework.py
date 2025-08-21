70f75ebd9ac921027aaad98a87d24ad7abdfa8aa
Valeriy Nasyrov <valerius.nasyrov@yandex.ru>
2025-08-21 18:56:12 +0300

    Исправил тест test_dicts, используя функцию zip

--- test_homework.py
+++ test_homework.py
@@ -89,7 +89,7 @@ def test_dicts():
     first = ["a", "b", "c", "d", "e"]
     second = [1, 2, 3, 4, 5]
     # TODO создайте словарь
-    d = {"a": 1, "b": 2, "c": 3, "d": 4, "e": 5}
+    d = dict(zip(first, second))
     print(first, second, d)
 
     assert isinstance(d, dict)
